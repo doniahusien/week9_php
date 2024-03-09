3134807c996c107486e890edd6233ab4664617b3
doniahusien <doniahusien@gmail.com>
2024-03-09 16:10:15 +0200

    new

--- Implementions/v53_to_v62/video58.php
+++ Implementions/v53_to_v62/video58.php
@@ -1,1 +1,24 @@
 <?php
+
+parse_str("name=hanin&email=hn@gmail.com",$query);
+echo "<pre>";
+print_r($query);
+echo "</pre>";
+
+echo $query["name"]."<br>";
+
+echo "Hello [] () * / .<br>"; 
+echo quotemeta("Hello [] () * / .<br>");
+
+
+echo str_pad("12",8,0,STR_PAD_BOTH). "<br>";
+echo str_pad("321" ,8,0,STR_PAD_BOTH). "<br>";
+echo str_pad("1313",8,0,STR_PAD_BOTH) . "<br>";
+echo str_pad("15435",8,0,STR_PAD_LEFT) . "<br>";
+echo str_pad("500432" ,8,0,STR_PAD_LEFT). "<br>";
+
+
+echo strtr("E@zero web schoo@","@","l")."<br>";
+
+$trArr = ["@" => "l", "#" => "o"];
+echo strtr("E@zer# web sch##@",$trArr)."<br>";
