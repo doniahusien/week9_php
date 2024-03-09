b3ab5713d70a2f7b477e851a9fbfc21e30ae5560
doniahusien <doniahusien@gmail.com>
2024-03-09 06:06:23 +0200

    new

--- Implementions/v53_to_v62/video54.php
+++ Implementions/v53_to_v62/video54.php
@@ -1,1 +1,14 @@
 <?php
+
+$str = "Sohir";
+
+echo lcfirst("Samia") . "<br>";
+echo ucfirst("samia") . "<br>";
+
+echo strtolower("SAMIA") . "<br>";
+echo strtoupper("samia") . "<br>";
+
+echo ucwords("sohir ahmed") . "<br>";
+echo ucwords("sohir,ahmed",",") . "<br>";
+
+echo str_repeat("Donia ",4);
---
+++ Implementions/v53_to_v62/video55.php
@@ -0,0 +1,28 @@
+<?php
+$str = "elzero web school Is cool";
+$names = ["Haneen", "Eman", "Rahma", "Zahraa"];
+
+echo implode(" ", $names);
+echo implode(", ", $names)."<br>";
+
+echo "<pre>";
+print_r(explode("I", $str));
+echo "</pre>";
+
+echo "<pre>";
+print_r(explode(" ", $str,3));
+echo "</pre>";
+
+echo "<pre>";
+print_r(explode(" ", $str,-3));
+echo "</pre>";
+
+echo str_shuffle("Ali")."<br>";
+echo strrev("Samia")."<br>";
+
+echo strlen(" Elzero   web  ")."<br>";
+echo trim(" Elzero   web  ")."<br>";
+echo strlen(trim(" Elzero   web  "))."<br>";
+echo trim("#  Elzero##","#")."<br>";
+echo rtrim("#  Elzero##","#")."<br>";
+echo ltrim("#  Elzero##","#")."<br>";
