4e0a4a5096e8025b5a8737d601eef3871b3cf41d
doniahusien <doniahusien@gmail.com>
2024-03-10 21:44:13 +0200

    new

--- Implementions/v53_to_v62/video61.php
+++ Implementions/v53_to_v62/video61.php
@@ -1,1 +1,12 @@
 <?php
+$str = "welcome to to elzero web web web school website very_very_very_long";
+echo wordwrap($str,8,"<br>",true);
+echo "<br>";
+
+echo ord("a")."<br>";
+echo chr(98)."<br>";
+
+echo similar_text("Elz_ero","Elz_eroo")."<br>";//7
+echo similar_text("Elz@ero","Elz_eroo")."<br>";//6
+echo similar_text("Elz@ero","Elz_eroo",$perc)."<br>";//6
+echo $perc;
