652adf36e84b54b4e27b3ffd13c7500d4220be3b
doniahusien <doniahusien@gmail.com>
2024-03-11 16:59:42 +0200

    new

---
+++ Implementions/v63_to_v72/video63.php
@@ -0,0 +1,33 @@
+<?php
+
+$friens=["haneen","samia","sohir","rahma","eman"];
+
+echo "<pre>";
+print_r(array_chunk($friens,3));
+echo "</pre>";
+
+
+$countries = ["Eg" => "Egypt", "KSA" => "Saudi Arabia", "Sy" => "Syria", "USA" => "United States"];
+
+echo "<pre>";
+print_r(array_chunk($countries,3,true));
+echo "</pre>";
+
+
+echo "<pre>";
+print_r(array_change_key_case($countries,CASE_LOWER));
+echo "</pre>";
+
+
+
+$keys = ["S", "H", "R"];
+$value = ["Samia", "Haneen", "Rahma"];
+echo "<pre>";
+print_r(array_combine($keys,$value));
+echo "</pre>";
+
+
+$counts = [1, 2, 3, 4, 4, 1, 2, 3, 1];
+echo "<pre>";
+print_r(array_count_values($counts));
+echo "</pre>";
---
+++ Implementions/v63_to_v72/video64.php
@@ -0,0 +1,33 @@
+<?php 
+$family=["Yousef","Faris","Mahmoud","Abdelrahman",["Rodina","Heba"]];
+
+echo "<pre>";
+print_r(array_reverse($family));
+print_r(array_reverse($family,true));
+print_r($family);
+echo "</pre>";
+
+
+
+$countries = ["Eg" => "Egypt", "KSA" => "Saudi Arabia", "Sy" => "Syria", "USA" => "United States"];
+echo "<pre>";
+print_r(array_flip($countries));
+echo "</pre>";
+
+
+$counting = ["A", "B", "C", "D", [1, 2]];
+echo count($counting)."<br>";//5
+echo count($counting,1)."<br>";//7
+
+
+$srch = [1, "2", 8, "6"];
+if(in_array(2, $srch,true)){
+    echo "IS FOUND <br>";
+}
+
+
+
+$countries = ["Eg" => "Egypt", "KSA" => "Saudi Arabia", "Sy" => "Syria", "USA" => "United States"];
+if (array_key_exists("KSA",$countries)){
+    echo "IS FOUND and full name is ".$countries["KSA"];
+}
