91afa5cd0b4e1cf23173da314bfb38ac8cedf083
doniahusien <doniahusien@gmail.com>
2024-03-19 10:33:48 +0200

    Header

--- Implementions/v98_to_v105/video105.php
+++ Implementions/v98_to_v105/video105.php
@@ -1,1 +1,9 @@
 <?php
+
+//header("HTTP/1.0 404 Not Found");
+//header("cache-control: no-cache, must-revalidate");
+//echo "<a href='test2.php'>Test</a>";
+
+//header("Location:test2.php");
+header("Refresh:5;url=test2.php");
+exit;
